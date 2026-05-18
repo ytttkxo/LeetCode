d3573beaacebf007250cc81dd65f22386d7f59a6
ytttkxo <ytttkxo03@gmail.com>
2026-05-18 16:04:47 +0200

    LC-length-of-last-word [Runtime: N/A; Memory: N/A]

---
+++ string/length-of-last-word.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+
+        // the length of the last word
+        int length = 0;
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        while(i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
